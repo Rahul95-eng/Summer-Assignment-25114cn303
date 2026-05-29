34b775efa2ef7a6b30fa516039f06b0290dbbba3
Rahul95-eng <dubeyrahul176739@gmail.com>
2026-05-29 20:47:01 +0530

    day2 uploaded

---
+++ day1/day2/palindrome.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+     int n, temp, rem, rev = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(n > 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    if(temp == rev)
+        printf("Palindrome number");
+    else
+        printf("Not palindrome number");
+
+    return 0;
+}
