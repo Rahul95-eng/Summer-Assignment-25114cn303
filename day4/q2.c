15a2338dda2ca907ef01d16ad971733e09b26128
Rahul95-eng <dubeyrahul176739@gmail.com>
2026-06-11 13:31:13 +0530

    day4 uploaded

---
+++ day4/q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a = 0, b = 1, c;
+
+    scanf("%d", &n);
+
+    if(n == 0)
+        printf("0");
+    else if(n == 1)
+        printf("1");
+    else
+    {
+        for(int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        printf("%d", b);
+    }
+
+    return 0;
+}
